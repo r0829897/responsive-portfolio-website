83d3cf0cd0a8b80e56b06c64724622a5303277b5
Yoran Mertens <yoran.mertens@student.kuleuven.be>
2021-07-21 00:59:44 +0200

    part 4

--- public/js/main.js
+++ public/js/main.js
@@ -13,7 +13,7 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add('close');
-    menu.classList.add('shwow');
+    menu.classList.add('show');
     menuNav.classList.add('show');
     menuBranding.classList.add('show');
     navItems.forEach(item => item.classList.add('show'));
@@ -23,7 +23,7 @@ function toggleMenu() {
   }
   else {
     menuBtn.classList.remove('close');
-    menu.classList.remove('shwow');
+    menu.classList.remove('show');
     menuNav.classList.remove('show');
     menuBranding.classList.remove('show');
     navItems.forEach(item => item.classList.remove('show'));
